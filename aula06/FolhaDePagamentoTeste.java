dcfed9b40aabc78391a6f1b2efd13ce1efeeb794
VitorSilvhan <vitor.s.conceicao6@aluno.senai.br>
2024-10-11 09:36:47 -0300

    Adcionado uma nova linha final

--- aula06/FolhaDePagamentoTeste.java
+++ aula06/FolhaDePagamentoTeste.java
@@ -14,7 +14,8 @@ public class FolhaDePagamentoTeste {
 		folha.setSalario2(2500f);
 		System.out.println("Nome funcionario2: " + folha.getNome2() + ", salario R$ " + folha.getSalario2());
 		System.out.println("=============================================");
-		System.out.println("Total da folha de pagamento R$ " + folha.totalFolhaPagamento(0, 0));	
+		System.out.println("Total da folha de pagamento R$ " + folha.totalFolhaPagamento(0, 0));
+		System.out.println("=============================================");
 
 	}
 
